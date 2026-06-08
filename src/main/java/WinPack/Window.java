a77368ceb23f2b4af8960e0480abd041bd4d9829
KAD207 <khoid3468@gmail.com>
2026-06-08 21:48:23 +0700

    window + simple key + mouse listener

---
+++ src/main/java/WinPack/Window.java
@@ -0,0 +1,124 @@
+package WinPack;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11C.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private float r, g, b, a;
+    private boolean fadeToBlack = false;
+
+    private static Window window = null;
+
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Skibidi";
+        r = 1;
+        g = 1;
+        b = 1;
+        a = 1;
+
+    }
+
+    public static Window get(){
+        if (Window.window == null){
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run(){
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+
+        // free memory cuz java suc
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // terminate GLFW and the free error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init(){
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // init GLFW
+        if (!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // create window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create GLFW WINDOW");
+        }
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        // make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // enable v-sync
+        glfwSwapInterval(1);
+
+        // make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    public void loop(){
+        while (!glfwWindowShouldClose(glfwWindow)){
+            // poll events
+            glfwPollEvents();
+
+            // clear bg color
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            if (fadeToBlack){
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+                a = Math.max(a - 0.01f, 0);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
+                fadeToBlack = true;
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)){
+                glfwSetWindowShouldClose(glfwWindow, true);
+            }
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
